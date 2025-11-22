7629e8348d497d93a6dfab4e6f06c6276d39bfff
Judson Neer <jud.neer@gmail.com>
2025-11-22 15:55:18 -0800

    First batch of verified wage determination records.

---
+++ tools/copy-wage-determinations.bash
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+cp data/wage-determinations/AZ/AZ20220031.0.json wage-determinations/0.4.0/
+cp data/wage-determinations/AZ/AZ20240039.7.json wage-determinations/0.4.0/
+# TODO HAS COMPLEXITY cp data/wage-determinations/CA/CA20240018.20.json wage-determinations/
+cp data/wage-determinations/CO/CO20230022.8.json wage-determinations/0.4.0/
+cp data/wage-determinations/GA/GA20220313.0.json wage-determinations/0.4.0/
+cp data/wage-determinations/ID/ID20230104.0.json wage-determinations/0.4.0/
+# TODO DOESNT PARSE cp data/wage-determinations/IN/IN20240002.27.json wage-determinations/
+# TODO HAS COMPLEXITY cp data/wage-determinations/MI/MI20240101.11.json wage-determinations/
+cp data/wage-determinations/MN/MN20240160.0.json wage-determinations/0.4.0/
+# TODO DOESNT PARSE cp data/wage-determinations/MO/MO20220050.3.json wage-determinations/
+# TODO DOESNT PARSE cp data/wage-determinations/NH/NH20200021.1.json wage-determinations/
+cp data/wage-determinations/NM/NM20210047.5.json wage-determinations/0.4.0/
+cp data/wage-determinations/NM/NM20240044.0.json wage-determinations/0.4.0/
+# TODO cp data/wage-determinations/NY/NY20210025.3.json wage-determinations/
+# TODO cp data/wage-determinations/NY/NY20240026.6.json wage-determinations/
+# TODO cp data/wage-determinations/NY/NY20240110.0.json wage-determinations/
+# TODO cp data/wage-determinations/NY/NY20250116.0.json wage-determinations/
+cp data/wage-determinations/OH/OH20220086.6.json wage-determinations/0.4.0/
+# TODO HAS COMPLEXITY cp data/wage-determinations/OR/OR20210031.0.json wage-determinations/
+# TODO HAS COMPLEXITY cp data/wage-determinations/OR/OR20180022.4.json wage-determinations/
+cp data/wage-determinations/PA/PA20240101.7.json wage-determinations/0.4.0/
+cp data/wage-determinations/TX/TX20220249.1.json wage-determinations/0.4.0/
+cp data/wage-determinations/TX/TX20230249.0.json wage-determinations/0.4.0/
+cp data/wage-determinations/TX/TX20240271.7.json wage-determinations/0.4.0/
+cp data/wage-determinations/TX/TX20240275.7.json wage-determinations/0.4.0/
+cp data/wage-determinations/UT/UT20240087.0.json wage-determinations/0.4.0/
+# TODO HAS COMPLEXITY cp data/wage-determinations/VA/VA20240162.1.json wage-determinations/
+cp data/wage-determinations/VT/VT20240025.2.json wage-determinations/0.4.0/
